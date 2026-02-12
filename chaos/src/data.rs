2c918694b55651a4bfb73d943346577452578da6
Vladimir Pak <z5686616@ad.unsw.edu.au>
2026-02-12 17:18:13 +1100

    completed data.rs

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -4,19 +4,39 @@ use serde::{Deserialize, Serialize};
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
 
+    let mut string_len = 0;
+    let mut int_sum = 0;
+
+    for val in request.data {
+        match val {
+            ReqVals::Word(string) => string_len += string.chars().count(),
+            ReqVals::Num(num) => int_sum += num,
+        }
+    }
+
     let response = DataResponse {
-        
+        string_len,
+        int_sum,
     };
 
     (StatusCode::OK, Json(response))
 }
 
+// Had to look up syntax for serde(untagged), the rest was without assistance
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum ReqVals {
+    Word(String),
+    Num(i32),
+}
+
 #[derive(Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<ReqVals>,
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: usize,
+    int_sum: i32,
 }
